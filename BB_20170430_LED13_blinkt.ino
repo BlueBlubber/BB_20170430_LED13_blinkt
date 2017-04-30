58eab1ae99568f116ebdc84318c33ba95fc6dc30
BlueBlubber <benjamin.birnbach@siemens-ppal.com>
2017-04-30 19:44:01 +0200

    Erklärung zugefügt
    Oben eine Erklärung zugefügt und unten im loop die Werte angepasst

---
+++ BB_20170430_LED13_blinkt.ino
@@ -0,0 +1,17 @@
+/*
+ * Programm um über den Wert von Pin 13 die interne LED am
+ * Arduino Uno zum Blinken zu bringen
+ */
+
+
+void setup() {               
+  pinMode(13, OUTPUT);   //Pin 13 soll Ausgang sein 
+}
+
+
+void loop() {
+  digitalWrite(13, HIGH);  //LED an
+  delay(2000);             // warte 2000ms
+  digitalWrite(13, LOW);   //LED aus
+  delay(200);              // warte 200ms
+}
